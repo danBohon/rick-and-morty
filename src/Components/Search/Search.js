2af3d647c6d45eb9253c76965c5f75d8503e65c9
Daniel Bohon <dan@Daniels-MacBook-Pro.local>
2018-10-06 17:18:38 -0700

    Fix Search Functionality

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,72 +1,104 @@
 import React, { Component } from 'react'
 import axios from "axios";
 
+import "./Search.css";
+
 import Results from "./Results/Results";
 
 export default class Search extends Component {
     constructor() {
         super();
         this.state = {
-                character: []
-            // name: [],
-            // species: [],
-            // status: [],
-            // image: [],
-            // id: []
+            character: [],
+            userInput: "",
+            allPages: []
         }
+        this.getAllData = this.getAllData.bind(this)
     }
+
+    handleChange(val) {
+        this.setState({ userInput: val })
+    }
+
+
+    componentDidMount() {
+        this.getAllData();
+    }
+
+   
+    getAllData() {
+    let allData = []
+        for (let i=1;i<=25;i++) {
+            axios.get(`https://rickandmortyapi.com/api/character/?page=${i}`)
+            .then(res => {
+                for (let j=0;j<res.data.results.length; j++) {
+                    allData.push(res.data.results[j]);
+                }
+            })
+        }
+        console.log("all data",allData)
+        this.setState({ allPages: allData })
+    }
+
+   
 
     getCharacter() {
-        axios.get("https://rickandmortyapi.com/api/character/?page=19").then(res => {
-            // console.log(res.data)
+        axios.get("https://rickandmortyapi.com/api/character/").then(res => {
+            console.log("getCharacter res.data",res.data)
             this.setState({
                 character: res.data.results
-                // name: res.data.results,
-                // species: res.data.species,
-                // status: res.data.status,
-                // image: res.data.image,
-                // id: res.data.id
-              })
+            })
+
+        // filterNames(e){
+        //     this.setState({users: this.state.store.filter(item => item.name.toLowerCase().includes(e.target.value.toLowerCase()))})
+        //   }
+        
+            // this.setState( {character: this.state.allPages.filter(person => (person.name === this.state.userInput) )});
+            this.setState( {character: this.state.allPages.filter(person => person.name.toLowerCase().includes(this.state.userInput.toLowerCase()) )});
         })
     }
+    
 
-    getAnotherCharacter() {
+    getRandomCharacter() {
         let id = Math.floor(Math.random() * (493 - 1) + 1)
         axios.get(`https://rickandmortyapi.com/api/character/${id}`).then(res => {
             console.table(res.data)
             this.setState({
-                character:[res.data]
+                character: [res.data]
               })
         })
-       
-        
     }
 
-  render() {
-    // const { image, name, species, status, id } = this.state;
-    const {character } =this.state
-    console.log(character);
-    
-    const result = character.map(n => {
-            return (
-                <div>        
-                    <Results 
-                    image={n.image}
-                    name={n.name}
-                    species={n.species}
-                    status={n.status}
-                    id={n.id}
-            />
-            </div>
-            )
-        })
+
+    render() {
+        const { character } = this.state
+        
+        const result = character.map(n => {
+                return (
+                    <div key={n.id}>        
+                        <Results 
+                        image={n.image}
+                        name={n.name}
+                        species={n.species}
+                        status={n.status}
+                        id={n.id}
+                        />
+                    </div>
+                )
+            })
         
     return (
         <div className="Component">
-                <h1>FIND SOMEONE</h1>
-                <button onClick={() => this.getCharacter()}>Get Character</button>
-                <button onClick={() => this.getAnotherCharacter()}>Get Random Character</button>
-                <div>{result}</div>
+                <h1>SHOW ME WHAT YOU GOT</h1>
+                <input 
+                    onChange={ (e) => this.handleChange(e.target.value) }
+                    placeholder="Rick Sanchez">
+                </input>
+                <button onClick={() => this.getCharacter()}>Search</button>
+                <button onClick={() => this.getRandomCharacter()}>Random Character</button>
+                <div>
+                    <div className="results">{result}</div>
+                </div>
                 {/* <Results 
                         image={character.image}
                         name={character.name}
